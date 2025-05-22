f49c07dccf0ee29b330d8454b6d5b61190cab51e
NguyenNamPHong03 <131447573+NguyenNamPHong03@users.noreply.github.com>
2025-05-22 23:41:30 +0700

    routes web

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,102 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AdminControllers\AdminPostControllers;
+use App\Http\Controllers\AdminControllers\DashboardControllers;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::get('/category/{id}', [PostController::class, 'byCategory'])->name('category.posts');
+
+// Trang settings
+Route::get('/profile/settings', [UserController::class, 'settings'])->name('profile.settings');
+
+// Cập nhật username & avatar
+Route::post('/profile/update-info', [UserController::class, 'updateInfo'])->name('profile.update.info');
+
+// Cập nhật mật khẩu
+Route::post('/profile/update-password', [UserController::class, 'updatePassword'])->name('profile.update.password');
+
+// Xoá tài khoản
+Route::delete('/profile/delete', [UserController::class, 'deleteAccount'])->name('profile.delete.account');
+// Route bình luận – CHO NGƯỜI DÙNG (KHÔNG PHẢI ADMIN)
+Route::middleware('auth')->group(function () {
+    Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
 });
+
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+
+Route::get('/categories', [CategoryController::class, 'showAllCategories'])->name('categories.all');
+
+
+
+Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
+
+Route::middleware('auth')->post('/comments', [CommentController::class, 'store'])->name('comments.store');
+
+
+
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+
+
+
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware(['auth', 'check_permissions'])
+    ->group(function () {
+
+    // Dashboard
+    Route::get('/', [DashboardControllers::class, 'index'])->name('dashboard');
+
+    // Bài viết
+    Route::prefix('posts')->name('posts.')->group(function () {
+        Route::get('/', [AdminPostControllers::class, 'index'])->name('index');
+        Route::get('/create', [AdminPostControllers::class, 'create'])->name('create');
+        Route::post('/store', [AdminPostControllers::class, 'store'])->name('store');
+        Route::get('/edit/{id}', [AdminPostControllers::class, 'edit'])->name('edit');
+        Route::post('/update/{id}', [AdminPostControllers::class, 'update'])->name('update');
+        Route::delete('/delete/{id}', [AdminPostControllers::class, 'destroy'])->name('destroy');
+    });
+
+    // User 
+    Route::prefix('users')->name('users.')->middleware(['is_admin'])->group(function () {
+        Route::get('/', [UserController::class, 'adminIndex'])->name('index');
+        Route::get('/{user}/edit', [UserController::class, 'adminEdit'])->name('edit');
+        Route::put('/{user}', [UserController::class, 'adminUpdate'])->name('update');
+        Route::delete('/{user}', [UserController::class, 'adminDestroy'])->name('destroy');
+    });
+    
+ Route::prefix('categories')->name('categories.')->middleware(['is_admin'])->group(function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('index');
+        Route::get('/create', [CategoryController::class, 'create'])->name('create');
+        Route::post('/store', [CategoryController::class, 'store'])->name('store');
+        Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('edit');
+        Route::post('/update/{id}', [CategoryController::class, 'update'])->name('update');
+        Route::delete('/delete/{id}', [CategoryController::class, 'destroy'])->name('destroy');
+    });
+    // Các nhóm route khác như categories, users,... bạn khai báo tương tự
+    Route::get('/comments', [CommentController::class, 'index'])->name('comments.index');
+    Route::delete('/comments/delete/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+});
+
