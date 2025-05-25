53389fe3bc9c0083f3a14da314c68e5d157e2e43
Duong DD <hohangungo@gmail.com>
2025-05-25 16:41:26 +0700

    sửa lại một số thứ

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+use App\Models\EmailNotification;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+
+    //take 5 oldest categories for header
+    public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+        // Categories (giữ nguyên)
+            $view->with('headerCategories', Category::orderBy('created_at', 'asc')->limit(5)->get());
+
+        });
     }
 }
