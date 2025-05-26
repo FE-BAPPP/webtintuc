d71c892aa1fb5a664e154b580566662c0ab23155
Duong DD <hohangungo@gmail.com>
2025-05-26 16:30:21 +0700

    sửa lại form login và register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('title', 'Sign in to LehyUI')
+@section('title', 'Đăng nhập vào BlogTFT')
 
 @section('content')
 <div class="login_container">
   <div class="login_left">
-    <p class="login_left_text">@lehyUI</p>
+    <p class="login_left_text">@BlogTFT</p>
     <video autoplay muted loop>
       <source src="https://cdn.dribbble.com/uploads/48226/original/b8bd4e4273cceae2889d9d259b04f732.mp4?1689028949" type="video/mp4">
     </video>
@@ -13,27 +13,27 @@
 
   <div class="login_right">
     <div class="login_form-container">
-      <h2 class="login_title">Sign in to LehyUI</h2>
+      <h2 class="login_title">Đăng nhập vào BlogTFT</h2>
 
       <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <label class="login_label">Username</label>
+        <label class="login_label">Tên người dùng</label>
         <input class="login_input" type="text" name="username" required />
 
-        <label class="login_label">Password</label>
+        <label class="login_label">Mật khẩu </label>
         <input class="login_input" type="password" name="password" required />
 
         <div class="login_forgot">
-          <a href="#" style="text-decoration: underline;">Forgot?</a>
+          <a href="#" style="text-decoration: underline;">Quên mật khẩu?</a>
         </div>
 
-        <button class="login_button" type="submit">Sign In</button>
+        <button class="login_button" type="submit">Đăng nhập</button>
       </form>
 
       <div class="login_signup">
-        Don't have an account?
-        <a href="{{ route('register') }}" style="text-decoration: underline">Sign Up</a>
+        Chưa có tài khoản?
+        <a href="{{ route('register') }}" style="text-decoration: underline">Đăng ký</a>
       </div>
     </div>
   </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('title', 'Sign up to LehyUI')
+@section('title', 'Đăng ký')
 
 @section('content')
 <div class="register_container">
   <div class="register_left">
-    <p class="register_left_text">@lehyUI</p>
+    <p class="register_left_text">@BlogTFT</p>
     <video autoplay muted loop>
       <source src="https://cdn.dribbble.com/uploads/48292/original/30fd1f7b63806eff4db0d4276eb1ac45.mp4?1689187515" type="video/mp4">
     </video>
@@ -13,37 +13,38 @@
 
   <div class="register_right">
     <div class="register_form-container">
-      <h2 class="register_title">Sign up to LehyUI</h2>
+      <h2 class="register_title">Đăng ký</h2>
 
       <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <label class="register_label">Username</label>
+        <label class="register_label">Tên đăng nhập</label>
         <input class="register_input" type="text" name="username" value="{{ old('username') }}" required />
 
-        <label class="register_label">Password</label>
+        <label class="register_label">Mật khẩu</label>
         <input class="register_input" type="password" name="password" required />
 
-        <label class="register_label">Confirm Password</label>
+        <label class="register_label">Xác nhận mật khẩu</label>
         <input class="register_input" type="password" name="password_confirmation" required />
 
-        <label class="register_label">Gender</label>
+        <label class="register_label">Giới tính</label>
         <div class="register_gender">
-          <label><input type="radio" name="sex" value="male" {{ old('sex') === 'male' ? 'checked' : '' }} required /> Male</label>
-          <label><input type="radio" name="sex" value="female" {{ old('sex') === 'female' ? 'checked' : '' }} required /> Female</label>
+          <label><input type="radio" name="sex" value="male" {{ old('sex') === 'male' ? 'checked' : '' }} required /> Nam</label>
+          <label><input type="radio" name="sex" value="female" {{ old('sex') === 'female' ? 'checked' : '' }} required /> Nữ </label>
         </div>
 
-        <button class="register_button" type="submit">Sign Up</button>
+        <button class="register_button" type="submit">Đăng ký</button>
       </form>
 
       <div class="register_terms">
-        By creating an account you agree with our
-        <a href="#">Terms of Service</a>, <a href="#">Privacy Policy</a>
+          Bằng việc tạo một tài khoản, bạn đồng ý với
+          <a href="#">Điều khoản dịch vụ</a>, <a href="#">Chính sách bảo mật</a>
       </div>
 
+
       <div class="register_signin">
-        Already have an account?
-        <a href="{{ route('login') }}">Sign In</a>
+        Đã có tài khoản?
+        <a href="{{ route('login') }}">Đăng nhập</a>
       </div>
     </div>
   </div>
